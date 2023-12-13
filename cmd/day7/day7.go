fe740196d10bdb00ddbcdc3cf09540913e6dc389
Bipin Maharjan <Bopon.maharjan@gmail.com>
2023-12-13 14:15:09 +0545

    day 7 part 2 wip

--- cmd/day7/day7.go
+++ cmd/day7/day7.go
@@ -36,6 +36,46 @@ func parseData(text string) map[string]int {
 	return data
 }
 
+// Part 1
+// func getTypeAndSimilarCount(word string) (string, int) {
+// 	counter := map[rune]int{}
+// 	for _, ch := range word {
+// 		counter[ch] += 1
+// 	}
+
+// 	max := 0
+// 	for _, val := range counter {
+// 		if max < val {
+// 			max = val
+// 		}
+// 	}
+
+// 	card_type := ""
+// 	switch max {
+// 	case 5:
+// 		card_type = const_five
+// 	case 4:
+// 		card_type = const_four
+// 	case 3:
+// 		if len(counter) == 2 {
+// 			card_type = const_fullHouse
+// 		} else {
+// 			card_type = const_three
+// 		}
+// 	case 2:
+// 		if len(counter) == 3 {
+// 			card_type = const_twoPair
+// 		} else {
+// 			card_type = const_onePair
+// 		}
+// 	default:
+// 		card_type = const_highCard
+// 	}
+
+// 	return card_type, max
+// }
+
+// Part 2
 func getTypeAndSimilarCount(word string) (string, int) {
 	counter := map[rune]int{}
 	for _, ch := range word {
@@ -43,10 +83,27 @@ func getTypeAndSimilarCount(word string) (string, int) {
 	}
 
 	max := 0
-	for _, val := range counter {
+	maxRune := '0'
+	for ch, val := range counter {
 		if max < val {
 			max = val
+			maxRune = ch
 		}
+	}
+
+	if maxRune == 'J' {
+		// get second max number
+		innerMax := 0
+		counter['J'] = 0
+		for _, val := range counter {
+			if innerMax < val {
+				innerMax = val
+			}
+		}
+
+		max += innerMax
+	} else {
+		max += counter['J']
 	}
 
 	card_type := ""
@@ -74,6 +131,24 @@ func getTypeAndSimilarCount(word string) (string, int) {
 	return card_type, max
 }
 
+// Part 1
+// var cardRank = map[rune]int{
+// 	'2': 1,
+// 	'3': 2,
+// 	'4': 3,
+// 	'5': 4,
+// 	'6': 5,
+// 	'7': 6,
+// 	'8': 7,
+// 	'9': 8,
+// 	't': 9,
+// 	'j': 10,
+// 	'q': 11,
+// 	'k': 12,
+// 	'a': 13,
+// }
+
+// Part 2
 var cardRank = map[rune]int{
 	'2': 1,
 	'3': 2,
@@ -84,7 +159,7 @@ var cardRank = map[rune]int{
 	'8': 7,
 	'9': 8,
 	't': 9,
-	'j': 10,
+	'j': 0,
 	'q': 11,
 	'k': 12,
 	'a': 13,
@@ -140,7 +215,7 @@ func sortCards(groupedCards map[string][]string) map[string][]string {
 	return groupedCards
 }
 
-func concatGroupedSortedCards(cards map[string][]string) []string{
+func concatGroupedSortedCards(cards map[string][]string) []string {
 	sortedCards := []string{}
 
 	if val, ok := cards[const_highCard]; ok {
@@ -171,19 +246,31 @@ func concatGroupedSortedCards(cards map[string][]string) []string{
 		sortedCards = append(sortedCards, val...)
 	}
 
-
 	return sortedCards
-
 }
 
 func part1(cards map[string]int) int {
 	groupedCards := groupCards(cards)
 	groupedSortedCards := sortCards(groupedCards)
-	sortedCards := concatGroupedSortedCards(groupedSortedCards);
+	sortedCards := concatGroupedSortedCards(groupedSortedCards)
 
 	total := 0
-	for i, key := range sortedCards{
-		mult := cards[key] * (i+1)
+	for i, key := range sortedCards {
+		mult := cards[key] * (i + 1)
+		total += mult
+	}
+
+	return total
+}
+
+func part2(cards map[string]int) int {
+	groupedCards := groupCards(cards)
+	groupedSortedCards := sortCards(groupedCards)
+	sortedCards := concatGroupedSortedCards(groupedSortedCards)
+
+	total := 0
+	for i, key := range sortedCards {
+		mult := cards[key] * (i + 1)
 		total += mult
 	}
 
@@ -200,7 +287,8 @@ func main() {
 
 	parsedData := parseData(text)
 
-	total := part1(parsedData)
+	// total := part1(parsedData)
+	total := part2(parsedData)
 
 	fmt.Println("Total: ", total)
 }
